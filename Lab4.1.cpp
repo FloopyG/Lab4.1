db2a73b7c63bfbc07f5b3c25eaee9ae409c5844d
FloopyG <36450035+FloopyG@users.noreply.github.com>
2020-10-24 20:23:05 +0300

    Initial commit

---
+++ Lab4.1.cpp
@@ -0,0 +1,54 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, N;
+    double S;
+
+    cout << "N = "; cin >> N;
+    S = 0;
+    i = N;
+
+    while (i <= 22)
+    {
+        S += sqrt(pow(i, 2) + pow(N, 2)) / i;
+        i++;
+    };
+
+    cout << S << endl;
+
+    S = 0;
+    i = N;
+
+    do {
+        S += sqrt(pow(i, 2) + pow(N, 2)) / i;
+        i++;
+    } while (i <= 22);
+
+    cout << S << endl;
+
+    S = 0;
+
+    for (i = N; i <= 22; i++) {
+        S += sqrt(pow(i, 2) + pow(N, 2)) / i;
+    };
+
+    cout << S << endl;
+
+    S = 0;
+
+    for (i = 22; N <= i; i--) {
+        S += sqrt(pow(i, 2) + pow(N, 2)) / i;
+    };
+
+    cout << S << endl;
+        
+
+    system("pause");
+    cin.get();
+    return 0;
+}
+
